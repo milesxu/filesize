68526c5885ae5de17ac554e296b1130facfb64c5
Ranger Alone <Ranger Alone>
2017-04-29 23:21:39 +0800

    mission complete

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 "use strict";
 let express = require('express');
+let parser = require('body-parser');
+let multer = require('multer');
+let uploader = multer();
+
+let app = express();
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+app.post('/upload', uploader.single('filename'), (req, res) => {
+    res.redirect('/filesize/' + req.file.size);
+});
+app.get('/filesize/:size', (req, res) => {
+    res.json({ size: req.params.size });
+});
+
+app.listen(process.env.PORT || 8080, () => console.log('running...'));
